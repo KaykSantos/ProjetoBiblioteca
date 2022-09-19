218da50ac10b9e5b38c9d26dc7eb2277c87d4100
KaykSantos <kaykarrudasantos2018@gmail.com>
2022-09-19 23:01:43 +0900

    Pagina conta e livro

--- www/components/script.js
+++ www/components/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.onload = function(){
     let url = 'http://bba.profrodolfo.com.br/php/livro.php';
     fetch(url)
@@ -12,43 +11,17 @@ window.onload = function(){
         for(let i = 0; i < json.length; i++){
             texto += '<section class="book-box">';
             texto += '<div class="img-livro">';
-            texto += '<img src="'+json[i].capa+'" alt="Capa do livro" height="80px">';
+            texto += '<img src="http://bba.profrodolfo.com.br/pages/imgs_capa/'+json[i].cd+'/'+json[i].capa+'" alt="Capa do livro" height="80px">';
             texto += '</div>';
             texto += '<h3>Título: '+json[i].titulo+' </h3>';
-            texto += '<h4>Autor: '+json[i].id_genero+'</h4>';
-            texto += '<h4>Editora: '+json[i].id_editora+'</h4> ';
-            texto += '<hr>';
+            texto += '<h4>Autor: '+json[i].Autor+'</h4>';
+            texto += '<h4>Editora: '+json[i].Editora+'</h4> ';
+            texto += '<h4 id="verLivro">Ver livro</h4>';
             texto += '</section>';
         }
         d.innerHTML += texto;
-    })
-.catch();   
-}
-=======
-window.onload = function(){
-    let url = 'http://bba.profrodolfo.com.br/php/livro.php';
-    fetch(url)
-    .then(resposta => {
-        return resposta.json();
-    })
-    .then(function(json){
-        let d = document.querySelector('#mainAcervo');
-        let texto = '';
 
-        for(let i = 0; i < json.length; i++){
-            texto += '<section class="book-box">';
-            texto += '<div class="img-livro">';
-            texto += '<img src="'+json[i].capa+'" alt="Capa do livro" height="80px">';
-            texto += '</div>';
-            texto += '<h3>Título: '+json[i].titulo+' </h3>';
-            texto += '<h4>Autor: '+json[i].id_genero+'</h4>';
-            texto += '<h4>Editora: '+json[i].id_editora+'</h4> ';
-            texto += '<hr>';
-            texto += '</section>';
-        }
-        d.innerHTML += texto;
     })
 .catch();   
 }
 
->>>>>>> db35cb53f507b4df1c1f8993a38269d8de196baa
